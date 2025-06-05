586a2058d3382fb6362c78e381e8f055dc011d30
attacker frnd <warriorsshadow39@gmail.com>
2025-06-05 15:08:43 +0530

    combining

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import Sidebar from "./sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata = {
@@ -21,9 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased`}>
         <div className="flex min-h-screen">
           <Sidebar />
           <main className="flex-1 bg-[#f8f9fb] min-h-screen p-0">{children}</main>
